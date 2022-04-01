c1c7b7649fe79452ea77ad234f4b406c63549ab7
ouyujyun <ouyujyun@mail.com>
2022-04-01 10:02:00 +0800

    HW401:建立新的ng專案

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './_page/login/login.component';
+import { IndexComponent } from './_page/index/index.component';
+import { PageAComponent } from './_page/page-a/page-a.component';
+import { PageBComponent } from './_page/page-b/page-b.component';
+import { HeaderComponent } from './_component/header/header.component';
+import { SidebarLeftComponent } from './_component/sidebar-left/sidebar-left.component';
+import { FooderComponent } from './_component/fooder/fooder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    IndexComponent,
+    PageAComponent,
+    PageBComponent,
+    HeaderComponent,
+    SidebarLeftComponent,
+    FooderComponent
   ],
   imports: [
     BrowserModule,
